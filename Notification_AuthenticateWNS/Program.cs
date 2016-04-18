4b7188a098d72ad4d05ed368cc0b7251b5fb1e3c
faiz.karim <faiz.karim@sprinklr.com>
2016-04-18 16:08:58 +0530

    get accessToken and send notification

---
+++ Notification_AuthenticateWNS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+namespace Notification_AuthenticateWNS
+{
+    class Program
+    {
+        private static string sid = "";
+        private static string secret = "";
+
+        static void Main(string[] args)
+        {
+            getAccessToken();
+        }
+      
+        public static void getAccessToken()
+        {
+            var urlEncodedSid = HttpUtility.UrlEncode(String.Format("{0}", sid));
+            var urlEncodedSecret = HttpUtility.UrlEncode(secret);
+
+            var body =
+              String.Format("grant_type=client_credentials&client_id={0}&client_secret={1}&scope=notify.windows.com", urlEncodedSid, urlEncodedSecret);
+
+            var client = new WebClient();
+            client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+            string response = client.UploadString("https://login.live.com/accesstoken.srf", body);
+           
+        }
+    }
+
+
+}
---
+++ Send_Notification/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Send_Notification
+{
+    class Program
+    {
+        static string URI = "";
+        static string accessToken = "";
+        static void Main(string[] args)
+        {
+            Push(URI, accessToken, "Hello 10");
+        }
+
+        public static HttpStatusCode Push(string pushUri, string accessToken, string text)
+        {
+            var subscriptionUri = new Uri(pushUri);
+
+            var request = (HttpWebRequest)WebRequest.Create(subscriptionUri);
+            request.Method = "POST";
+            request.ContentType = "text/xml";
+            request.Headers = new WebHeaderCollection();
+            request.Headers.Add("X-WNS-Type", "wns/toast");
+            request.Headers.Add("Authorization", "Bearer " + accessToken);
+
+            string data = "<?xml version='1.0' encoding='utf-8'?>" + GetToastMessage();
+
+            byte[] notificationMessage = Encoding.Default.GetBytes(data);
+            request.ContentLength = notificationMessage.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(notificationMessage, 0, notificationMessage.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            return response.StatusCode;
+        }
+
+        private static string GetToastMessage()
+        {
+            return @"<toast>
+                        <visual lang=""en-US"">
+                            <binding template=""ToastText01"">
+                                <text id=""1"">New test arrived!</text>
+                            </binding>  
+                        </visual>
+                    </toast>";
+        }
+    }
+}
